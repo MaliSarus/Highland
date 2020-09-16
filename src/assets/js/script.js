2a117310bddcb19ea51180f24f1ae32893020f96
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-09-16 18:09:12 +0300

    fixes 1 page

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -286,22 +286,37 @@
 
             jobFormBackgroundWidth();
         }
-        if (isSet($('.compas-list'))){
+        if (isSet($('.compas-list'))) {
             var img = $('.compas__arrow');
-            if(img.length > 0){
+            if (img.length > 0) {
                 var offset = img.offset();
-                function mouse(evt){
-                    var center_x = (offset.left) + (img.width()/2);
-                    var center_y = (offset.top) + (img.height()/2);
-                    var mouse_x = evt.pageX; var mouse_y = evt.pageY;
-                    var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
-                    var degree = (radians * (180 / Math.PI) * -1) + 180;
-                    img.css('-moz-transform', 'translate(-50%,  -50%) rotate('+degree+'deg)');
-                    img.css('-webkit-transform', 'translate(-50%,  -50%) rotate('+degree+'deg)');
-                    img.css('-o-transform', 'translate(-50%,  -50%) rotate('+degree+'deg)');
-                    img.css('-ms-transform', 'translate(-50%,  -50%) rotate('+degree+'deg)');
+                var compasList = $('.compas-list__text ul');
+
+                function imageRotate(degree) {
+                    img.css('-moz-transform', 'translate(-50%,  -50%) rotate(' + degree + 'deg)');
+                    img.css('-webkit-transform', 'translate(-50%,  -50%) rotate(' + degree + 'deg)');
+                    img.css('-o-transform', 'translate(-50%,  -50%) rotate(' + degree + 'deg)');
+                    img.css('-ms-transform', 'translate(-50%,  -50%) rotate(' + degree + 'deg)');
                 }
-                $(document).mousemove(mouse);
+
+                var oldDegree = 0;
+                compasList.on('mouseenter', 'li', function () {
+                    var center_x = (offset.left) + (img.width() / 2);
+                    var center_y = (offset.top) + (img.height() / 2);
+                    var mouse_x = $(this).offset().left;
+                    var mouse_y = $(this).offset().top + $(this).height() / 2;
+                    var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+                    var degree = (radians * (180 / Math.PI) * -1) + 90;
+                    var eDegree = Math.abs(degree - oldDegree) / 2;
+                    imageRotate(degree - eDegree);
+                    setTimeout(function () {
+                        imageRotate(degree + eDegree)
+                        setTimeout(function () {
+                            imageRotate(degree)
+                        },200)
+                    },200)
+
+                })
             }
 
         }
