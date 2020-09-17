73b650f0f650bdc342fe4cf8e682cf7471f2fbc9
MaliSarus <43217693+MaliSarus@users.noreply.github.com>
2020-09-17 22:28:41 +0300

    fixes

--- src/index.php
+++ src/index.php
@@ -3,13 +3,13 @@ require './modules/header.php';
 
 // 1 СТРАНИЦА
 
-require './modules/top-slider.php';
-require './modules/quote-and-text.php';
-require './modules/tabs.php';
-require './modules/pic-and-text.php';
-require './modules/logo-slider.php';
-require './modules/contact-form.php';
-require './modules/job-form.php';
+//require './modules/top-slider.php';
+//require './modules/quote-and-text.php';
+//require './modules/tabs.php';
+//require './modules/pic-and-text.php';
+//require './modules/logo-slider.php';
+//require './modules/contact-form.php';
+//require './modules/job-form.php';
 
 // --1 СТРАНИЦА
 
