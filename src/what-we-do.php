e772e6170074de42feb15c9ecb165536b8a19f66
MaliSarus <43217693+malisarus@users.noreply.github.com>
2020-09-22 11:30:34 +0300

    FIXES

--- src/what-we-do.php
+++ src/what-we-do.php
@@ -1,5 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Заголовок</title>
+    <link rel="stylesheet" href="./assets/css/swiper.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script defer src="./assets/js/jquery-3.4.1.min.js"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+    <!--    <script defer src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/CustomEase.min.js"></script>-->
+    <script defer src="./assets/js/swiper.min.js"></script>
+    <script defer src="./assets/js/script.js"></script>
+</head>
+<body>
+<header id="header" class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 d-flex align-items-center justify-content-between">
+                <div class="header__logo">
+                    <a href="index.php">
+                        <img src="./assets/img/interface/logo.svg" alt="">
+                    </a>
+                </div>
+                <button class="hamburger hamburger--collapse" type="button">
+                  <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                  </span>
+                </button>
+            </div>
+            <div class="col-md-9 d-flex justify-content-end">
+                <nav class="header__menu">
+                    <ul>
+                        <li><a href="about-us.php">Our Firm</a></li>
+                        <li class="active"><a href="what-we-do.php">What we Do</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<main>
 <?php
-require './modules/header.php';
+//require './modules/header.php';
 
 // 3 СТРАНИЦА
 ?>
